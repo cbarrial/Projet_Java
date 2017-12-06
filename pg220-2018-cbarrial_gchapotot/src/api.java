75b5c0153cb07a7513058b16468793e559c13d0c
gchapotot <chapotot.guillaume@gmail.com>
2017-12-06 12:17:39 +0100

    06/11

--- pg220-2018-cbarrial_gchapotot/src/api.java
+++ pg220-2018-cbarrial_gchapotot/src/api.java
@@ -1,10 +1,12 @@
 
 import java.io.BufferedReader;
+
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.json.*;
 
-public class api extends temp{
+public class api extends api1{
  
  public static void main(String[] args) throws Exception {
  
@@ -23,28 +25,62 @@ public class api extends temp{
  // HTTP GET request
  private void sendingGetRequest() throws Exception {
  
-  String urlString = "https://www.metaweather.com/api/location/44418";
+  String urlString = "https://www.metaweather.com/api/location/580778";
+  String urlString2 = "https://www.prevision-meteo.ch/services/json/bordeaux";
   
   URL url = new URL(urlString);
   HttpURLConnection con = (HttpURLConnection) url.openConnection();
+  URL url2 = new URL(urlString2);
+  HttpURLConnection con2 = (HttpURLConnection) url2.openConnection();
  
  
   // Reading response from input Stream
   BufferedReader in = new BufferedReader(
           new InputStreamReader(con.getInputStream()));
+  BufferedReader in2 = new BufferedReader(
+          new InputStreamReader(con2.getInputStream()));
+  
   String output;
-  String response=null;
+  String response="";
+  String output2;
+  String response2="";
+  
  
   while ((output = in.readLine()) != null) {
    response += output;
   }
   in.close();
+  while ((output2 = in2.readLine()) != null) {
+	   response2 += output2;
+	  }
+	  in2.close();
+	  
+	
+
   
-  temp T= new temp();
-  
+// first api
   //printing result from response
   System.out.println(response.toString());
-  System.out.println(T.Temperature(response));
+  JSONObject info = new JSONObject(response);
+  JSONArray nam =info.getJSONArray("consolidated_weather");
+  JSONObject temp = nam.getJSONObject(0);
+  
+  System.out.println((temp.getInt("max_temp")+temp.getInt("min_temp"))/2); 
+  
+//second api
+  System.out.println(response2.toString());
+  JSONObject info2 = new JSONObject(response2);
+  JSONObject temp2 =info2.getJSONObject("fcst_day_0");
+  
+  
+  System.out.println((temp2.getInt("tmax")+temp2.getInt("tmin"))/2); 
+ 
+  
+  JSONArray tab=Temperature ("bordeaux");
+  System.out.println(tab.toString());
+  
+  
+  
  
  }
 }
---
+++ pg220-2018-cbarrial_gchapotot/src/api1.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.*;
+
+public class api1 {
+	String city;
+	JSONArray tab;
+	
+	
+	public JSONArray Temperature (String city) throws Exception {
+		JSONArray tab = new JSONArray();
+		
+			String urlString="https://www.metaweather.com/api/location/search/?query="+city;
+			URL url = new URL(urlString);
+			  HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			  
+			// Reading response from input Stream
+			 BufferedReader in = new BufferedReader(
+			       new InputStreamReader(con.getInputStream()));
+			 
+			 String output;
+			 String response="";
+			 
+			 
+			 while ((output = in.readLine()) != null) {
+				   response += output;
+				  }
+				  in.close();
+				  
+			JSONArray nam =new JSONArray(response);
+			JSONObject temp = nam.getJSONObject(0);
+				  
+			int woeid=temp.getInt("woeid");
+			
+		
+		String 	urlString1="https://www.metaweather.com/api/location/"+Integer.toString(woeid);
+		URL url1 = new URL(urlString1);
+		  HttpURLConnection con1 = (HttpURLConnection) url1.openConnection();
+		  
+		// Reading response from input Stream
+		 BufferedReader in1 = new BufferedReader(
+		       new InputStreamReader(con1.getInputStream()));
+		 
+		 String output1;
+		 String response1="";
+		 
+		 
+		 while ((output1 = in1.readLine()) != null) {
+			   response1 += output1;
+			  }
+			  in1.close();
+			  
+	
+		JSONObject info = new JSONObject(response1);
+	    JSONArray nam1 =info.getJSONArray("consolidated_weather");
+	    for (int i=0;i<5;i++) {
+	    		tab.put(nam1.getJSONObject(i));	
+	    		
+	    }
+		  
+		
+		return tab;
+	}
+	
+}
